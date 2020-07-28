aa6f274299228ff06c34555155047a13416d4a43
Andrey Radchishin <andrey@andreyrd.com>
2020-07-27 22:20:31 -0700

    add missing

---
+++ src/views/Blog.vue
@@ -0,0 +1,21 @@
+<template>
+	<div class="blog">
+		<div class="post">
+			<router-link to="/blog/what-does-it-mean-to-be-worldly" class="feature">
+				<Feature :img="require('@/assets/blog.jpg')" text="What does it mean to be worldly?" />
+			</router-link>
+		</div>
+	</div>
+</template>
+
+<script>
+import Feature from '@/components/Feature.vue';
+
+export default {
+	components: { Feature },
+};
+</script>
+
+<style>
+
+</style>
---
+++ src/views/BlogPost.vue
@@ -0,0 +1,105 @@
+<template>
+	<div class="blog post">
+		<Feature :img="require('@/assets/blog.jpg')" text="What does it mean to be worldly?" size="small" />
+		<div class="author">Daniel Bozhko</div>
+
+		<p class="quote">
+			1 John 2:15 Do not love the world or the things in the world. If anyone loves the world, the love of the Father is not in him. (ESV)
+		</p>
+
+		<p>
+			Have you ever thought about this verse? If you have been in church for any amount of time, you have probably heard this verse before (although I probably shouldn’t make any assumptions). When I was younger I had an interpretation of what this meant. It wasn’t an incorrect interpretation, but it also wasn’t complete. When I was younger, I understood that loving the world is: drugs, drinking, and sex outside of marriage. Now that isn’t wrong, per se, but it isn’t everything that John is saying through this text. Because drugs, drinking and extramarital sex are all part of a system. This system is the world. There’s a reason that all these things exist in the world, and, especially right now, no one ever looks down on it. On the contrary, people praise this behavior.
+		</p>
+
+		<p>
+			So what is worldliness? It isn’t just ignoring the universal moral law of God. No, it is something more. It is something that has been happening all throughout history. It is a rewriting of the universal moral law. Worldliness is taking the moral law, redefining it, and making it mean something else. It’s a totally new morality.
+		</p>
+
+		<p>
+			See, it would be fine if people just said “I believe what I believe, and you believe what you believe, and let’s just leave it at that.” But no! That is not what happens. The world creates a whole new set of moral standards. What are these standards based off? Simple, John says, “…the lust of the flesh, the lust of the eyes and the pride of life…” (NIV) People rewrite the rule book based on what they want to do.
+		</p>
+
+		<p>
+			Now like I said, it would be all well and good if everyone just left everyone alone and then followed the government rules and that’s it. But no, it never works that way. Morality is not an individual thing. It is impossible for me to believe one thing and for you to believe something else, because humanity inherently understands that morality is universal. So when the world creates its own morality that justifies its own lusts and desires, it then begins to force everyone to comply with that moral code. That new morality now becomes universal. It has to. It feels forced to. And that is worldliness.
+		</p>
+
+		<p>
+			I want to make a comment on this whole “moral change” thing. Like I said, this has been happening throughout history. It happens not just in a secular setting; it also happens in religious settings. Churches often do the same thing. Churches create traditions that at first are good things. It could be something as simple as standing during the reading of the Word of God. But then a few years go by and a new generation grows up that has only experienced standing up during the reading of the Word of God and nothing else. Now this tradition has made deep roots. What happens then if during some service a preacher doesn’t call the people to stand? Or what if someone begins changing this tradition so that the people continue to sit during the reading? If you’ve gone to church long enough, you have probably experienced this. This tradition becomes doctrine. And as soon as someone tries to change it he gets shut down, labelled a heretic that is bringing wrong teaching into the church, and essentially gets booted out of the church because of his “false teaching.”
+		</p>
+
+		<p>
+			The world does the same thing. Except, instead of conserving traditions of the past, it tends to progress very quickly. In fact, progress is the main tradition; it is the primary doctrine of the worlds thinking. So, when you don’t abide by the world’s tradition, when you are countercultural, the world rejects you, shuts you down, and continues moving forward in their goal of “transforming society” through the gradual erosion of God’s universal moral law.
+		</p>
+
+		<p>
+			This eventually leads to a sort of anachronistic moralism. As defined by the Merriam- Webster dictionary, anachronism means “a chronological misplacing of persons, events, objects, or customs in regard to each other”.<sup>1</sup> Anachronism can often be used for comedic purposes: like having an ancient character drink from a plastic water bottle (different time periods). Anachronistic moralism is taking modern day “virtues” and judging people who lived in other centuries according to our modern day concepts. For example, feminism didn’t exist in the 18th century. So to call the founding fathers patriarchal bigots because they didn’t have a woman in first continental congress is anachronistic.
+		</p>
+
+		<p>
+			But it isn’t that important to be consistent in the way we judge nowadays. Because the accepted form of justice today allows for anachronistic moralism to exist. In fact, it is celebrated. That is why there are those out there destroying statues of George Washington. Because according to the modern moral standard he is a sinner. Forget a law that is universal and timeless. Nope. Our law grows, it never stays the same, because “progress.”
+		</p>
+
+		<p>
+			This is worldliness. And do you know why it is so tempting to fall prey into the tricks of the world? Because we hate being hated. And this is where the love of the world comes into play. We love being loved by the world, and when the world does not love us we feel out of place. And so we conform. We begin changing our positions so that we appear to be morally righteous, forgetting all along that the moral standard that we are conforming to is not of God but of the world. Which means it is of the devil.
+		</p>
+
+		<p class="quote">
+			Isaiah 5:20 Woe to those who call evil good<br>
+			and good evil,<br>
+			who put darkness for light<br>
+			and light for darkness,<br>
+			who put bitter for sweet<br>
+			and sweet for bitter! (ESV)
+		</p>
+
+		<p>
+			This is the world. The world’s morality calls evil good. But what we don’t think about often is how it also calls good evil. I think that might be even worse. Society today has become legalistic in their own way. It has made the destruction of families easy and and considers it good that husbands and wives divorce, even if it might result in terrible consequences for the children. They have made homosexuality good. They have allowed men to call themselves women and women to call themselves men. They applaud the killing of unborn babies, giving standing ovations when laws are passed that allow for babies to be killed up to the moment of birth. And we could keep going.
+		</p>
+
+		<p>
+			But at the same time they condemn things that the Bible doesn’t condemn, and in fact condemn things that the Bible calls good. Sex inside of marriage is made fun of and outside of marriage it is celebrated. The submission of wives to husbands is called abusive and is ridiculed. Christian education is called dangerous indoctrination. Purity is called prudish. Having children is considered dangerous to the environment. And most importantly, belief in God is labeled as stupid, and organized religion is said to be “… a sham and a crutch for weak-minded people…”<sup>2</sup>.
+		</p>
+
+		<p>
+			So now consider this for yourself, are you enticed by the worlds claims? Because the claims are enticing. When you love the world, you love its approval. If you, a Christian, still care about what the world says I encourage you today to stop caring. It will never fully accept you. It will never accept your repentance for wrongdoing or wrong thinking. And if you are ever found out to be “on the wrong side of history”, you will be mocked, de-platformed, and cancelled. Stop playing that game. The morality of the world is constantly moving, it is not objective, and there is no way to always meet its needs. Stop compromising God’s desires for the world’s desires. Start compromising the worlds desires for the sake of doing God’s desires. And when you fail, if you come in repentance, he is faithful and just. He will forgive you of your sin. He will accept you as a son or daughter because Jesus Christ, His son, has justly paid your debt on the cross. And this is objective. It isn’t an ever-moving target. It is true and nothing in this world will make it untrue.
+		</p>
+
+		<div class="sources">
+			<p>1. <a href="https://www.merriam-webster.com/dictionary/anachronism">https://www.merriam-webster.com/dictionary/anachronism</a></p>
+			<p>2. Jesse Ventura</p>
+		</div>
+	</div>
+</template>
+
+<script>
+import Feature from '@/components/Feature.vue';
+
+export default {
+	components: { Feature },
+};
+</script>
+
+<style lang="scss" scoped>
+.post {
+	padding-bottom: 64px;
+}
+
+.author {
+	margin: 8px 0 16px;
+	text-align: right;
+}
+
+.quote {
+	margin-left: 16px;
+	padding-left: 16px;
+	border-left: 1px solid white;
+}
+
+.sources {
+	font-size: 14px;
+
+	p {
+		margin: 0;
+	}
+}
+</style>
